1495ac662adcb38e5943e2b4d248190d91e04a50
evamarie25 <github@eva.stebich.de>
2024-12-03 11:44:25 +0100

    added comments

--- Main.java
+++ Main.java
@@ -15,26 +15,30 @@ public class Main {
 
     public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int move;
+		int number; //die zu tauschende Zahl
 		
-		int moves = 0;
+		int moves = 0; //Spielzüge
 		
-		createPuzzle(3);
+		createPuzzle(3); //das zweidimensionale Array sortiert erstellen
 		
 		puzzleSolved = puzzle;
         
-		mixPuzzle(60);
+		mixPuzzle(60); //Array mit n zügen mischen
         
 		System.out.println("OK, es kann losgehen!");
 
+		//solange isGameOver != true wird der Spieler aufgefordert Zahlen einzugeben 
+		//und das Puzzel wird auf der Konsole ausgegeben
 		while(!isGameOver()){
 			printPuzzle();
 			
+			//Eingabe des Spielers
 			System.out.print("Bitte geben sie die zu tauschende Zahl ein: ");
-			move = scan.nextInt();
+			number = scan.nextInt();
 			System.out.println("");
-						
-			swapFields(move);
+			
+			//Methode um die einegegebene Zahl mit dem leeren Feld zu tauschen
+			swapFields(number);
 			
 			moves++;
 			
@@ -49,12 +53,14 @@ public class Main {
 
 	public static void createPuzzle(int size) {
 		setSize(size);
-		puzzle = new int[size][size];
+		puzzle = new int[size][size]; //neues zweidimensionales Array mit der angegebenen grösse erstellen
 		for(int i=0; i<size; i++){
 			for(int j=0; j<size; j++){
+				//das Array wird aufsteigend mit den Zahlen 1-8 befüllt
 				puzzle[j][i] = 1 + j*size + i;
 			}
 		}
+		//der Index des leeren Feldes wird in x und y gespeichert
 		x = size-1;
 		y = size-1;
 		puzzle[x][y] = 0;
@@ -63,8 +69,10 @@ public class Main {
 	public static void mixPuzzle(int n) {
 		int tmp;
 		
+		//bis n erreicht wird, wird eine random Zahl von 0-3 ermittelt und das puzzel wird dem Fall entsprechend nach oben, unten, links oder rechts "verschoben"
 		for(int i = 0; i <= n; i++){
 			switch((int)Math.floor(Math.random()*4)){
+				//nach oben
 				case 0:{
 					if(y < 1) break;
 					tmp = puzzle[y][x];
@@ -74,6 +82,7 @@ public class Main {
 					break;
 
 				}
+				//nach links
 				case 1:{
 					if(x < 1) break;
 					tmp = puzzle[y][x];
@@ -82,6 +91,7 @@ public class Main {
 					x--;
 					break;
 				}
+				//nach unten
 				case 2:{
 					if(y > 1) break;
 					tmp = puzzle[y][x];
@@ -90,6 +100,7 @@ public class Main {
 					y++;
 					break;
 				}
+				//nach rechts
 				case 3:{
 					if(x > 1) break;
 					tmp = puzzle[y][x];
@@ -123,7 +134,7 @@ public class Main {
 
     public static void swapFields(int number){
 		int tmp;
-		getFieldIndex(number);
+		getFieldIndex(number); //die Position der eingegebenen number wird ermittelt
 		//wenn die Zahl über dem leeren Feld steht
 		if (y > yFilled && x == xFilled){
 			tmp = puzzle[y][x];
@@ -147,7 +158,7 @@ public class Main {
 			puzzle[y+1][x] = tmp;
 			y++;
 		}	
-
+		//wenn die Zahl links von dem leeren Feld steht
 		else if (x > xFilled && y == yFilled){
 			tmp = puzzle[y][x];
 			puzzle[y][x] = puzzle[y][x-1];
@@ -161,6 +172,7 @@ public class Main {
     }
 
 	public static boolean isGameOver(){
+		//ueberpruefung ob das Array wieder aufsteigend sortiert ist, false wenn der wert nicht i+1 entspricht
 		for(int i=0; i<size; i++){
 			for(int j=0; j<size; j++){
 				if(puzzle[j][i] == 0) continue;
@@ -172,6 +184,7 @@ public class Main {
 	}
 
 	public static void getFieldIndex(int number){
+		//wenn die uebergebene number dem wert an puzzle[i][j] entspricht, wird der index in xFilled und yFilled gespeichert
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < puzzle[i].length; j++) {
 				if (puzzle[i][j] == number){
